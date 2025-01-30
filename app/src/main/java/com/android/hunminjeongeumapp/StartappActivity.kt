b92247848bd39cca908716f2db46387390b2babf
DESKTOP-11JE3UK\mehua <mehua2683@gmail.com>
2025-01-30 21:08:04 +0900

    feat: 앱 시작 화면 추가 / fix: AndroidManifest.xml 코드 수정 (스타트 화면 지정 변경)

---
+++ app/src/main/java/com/android/hunminjeongeumapp/StartappActivity.kt
@@ -0,0 +1,21 @@
+package com.android.hunminjeongeumapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class StartappActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_startapp)
+
+        // button_StartApp 클릭 시 MainActivity로 이동
+        val buttonStartApp = findViewById<Button>(R.id.button_StartApp)
+        buttonStartApp.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // 현재 액티비티 종료
+        }
+    }
+}
