d37fd762fcccd28cfc838b66a3af028ffb2f3488
DESKTOP-11JE3UK\mehua <mehua2683@gmail.com>
2025-01-30 17:21:17 +0900

    feat: 메인에서 초성게임, 크래프트 화면 연결

---
+++ app/src/main/java/com/android/hunminjeongeumapp/CraftActivity.kt
@@ -0,0 +1,11 @@
+package com.android.hunminjeongeumapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CraftActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_craft)
+    }
+}
--- app/src/main/java/com/android/hunminjeongeumapp/MainActivity.kt
+++ app/src/main/java/com/android/hunminjeongeumapp/MainActivity.kt
@@ -1,15 +1,32 @@
 package com.android.hunminjeongeumapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.hunminjeongeumapp.quiz_a.QuizAActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_craft_page)
+        setContentView(R.layout.activity_main)
 
+        // 버튼 초기화
+        val buttonToCGame = findViewById<Button>(R.id.button_to_C_Game)
+        val buttonToCraft = findViewById<Button>(R.id.button_to_Craft)
+
+        // 버튼 클릭 시 액티비티 이동
+        buttonToCGame.setOnClickListener {
+            val intent = Intent(this, QuizAActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonToCraft.setOnClickListener {
+            val intent = Intent(this, CraftActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
